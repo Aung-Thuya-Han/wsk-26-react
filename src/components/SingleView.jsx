fb79cb251e9e1e71b6fbb0f1dd920490632e68c8
aungthuyahan <aungthu.ya659@gmail.com>
2026-09-17 15:37:29 +0300

    Add open attribute to dialog in SingleView and include close button

--- src/components/SingleView.jsx
+++ src/components/SingleView.jsx
@@ -2,9 +2,19 @@ const SingleView = (props) => {
   const media = props.media;
 
   return (
-    <dialog>
+    <dialog open>
       <h1>{media.title}</h1>
 
+      <div>
+        <button
+          onClick={() => {
+            props.setSelectedItem(null);
+          }}
+        >
+          x
+        </button>
+      </div>
+      
       <img
         src={media.filename}
         alt={media.description}
