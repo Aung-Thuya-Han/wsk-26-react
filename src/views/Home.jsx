a98471a8ce8c768bcc19135a56e14bbdd142b1dd
aungthuyahan <aungthu.ya659@gmail.com>
2026-09-23 21:26:38 +0300

    Refactor Home component to use MediaRow for rendering media items and update Vite configuration base path to custom-hooks

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,47 +1,73 @@
 import {useState} from 'react';
-import MediaRows from '../components/MediaRows';
+import MediaRow from '../components/MediaRow';
 import SingleView from '../components/SingleView';
 import {useMedia} from '../hooks/apiHooks';
+
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState(null);
   const {mediaArray} = useMedia();
 
   return (
-    <>
-      <h2>My Media</h2>
+    // <>
+    //   <h2>My Media</h2>
 
-      {selectedItem ? (
-        <SingleView
-          media={selectedItem}
-          setSelectedItem={setSelectedItem}
-        />
-      ) : (
-        ''
-      )}
+    //   {selectedItem ? (
+    //     <SingleView
+    //       media={selectedItem}
+    //       setSelectedItem={setSelectedItem}
+    //     />
+    //   ) : (
+    //     ''
+    //   )}
 
-      <table>
-        <thead>
-          <tr>
-            <th>Thumbnail</th>
-            <th>User</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Created</th>
-            <th>Size</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <MediaRows
-            items={mediaArray}
-            setSelectedItem={setSelectedItem}
-            selectedItem={selectedItem}
-          />
-        </tbody>
-      </table>
-    </>
-  );
-};
+    //   <table>
+    //     <thead>
+    //       <tr>
+    //         <th>Thumbnail</th>
+    //         <th>User</th>
+    //         <th>Title</th>
+    //         <th>Description</th>
+    //         <th>Created</th>
+    //         <th>Size</th>
+    //         <th>Type</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       <MediaRows
+    //         items={mediaArray}
+    //         setSelectedItem={setSelectedItem}
+    //         selectedItem={selectedItem}
+    //       />
+    //     </tbody>
+    //   </table>
+    // </>
+
+    
+         <>
+             {selectedItem ? (
+              <SingleView
+                media={selectedItem}
+                setSelectedItem={setSelectedItem}
+              />
+            ) : (
+              ''
+            )}
+
+             <table>
+                 <tbody>
+                     {mediaArray.map((mediaItem) => (
+                         <MediaRow
+                             key={mediaItem.media_id}
+                             media={mediaItem}
+                             setSelectedItem={setSelectedItem}
+                         />
+                     ))}
+                 </tbody>
+             </table>
+         </>
+     );
+ };
+
 
 export default Home;
